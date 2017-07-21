cb576fb1d0a0430474d890cbc1756ba9ef2c9bca
rishabh <rishabh.gupta@smartdatainc.net>
2017-07-21 18:53:12 +0530

    strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LocationStrategy, HashLocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { HttpModule } from '@angular/http';
@@ -8,12 +8,14 @@ import { LoginComponent } from './login/login/login.component';
 import { LoaderComponent } from './shared/loader/loader.component';
 import { RegistrationComponent } from './login/registration/registration.component';
 import { FormsModule }   from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     LoaderComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +23,10 @@ import { FormsModule }   from '@angular/forms';
     HttpModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { RegistrationComponent } from './login/registration/registration.component';
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
 {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'login',
